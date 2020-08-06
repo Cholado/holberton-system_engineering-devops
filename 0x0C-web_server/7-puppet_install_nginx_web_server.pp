591b08cf6ebfb234ba310913d7e1f704dc5e5fb4
Cholado <sntgmtnz@gmail.com>
2020-08-05 19:47:50 -0500

    newline in string 404, puppet lint error

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -16,8 +16,8 @@ content => 'Holberton School',
 }
 
 file_line { 'Add redirection, 301':
-path   => '/etc/nginx/sites-available/default',
 ensure => 'present',
+path   => '/etc/nginx/sites-available/default',
 after  => 'listen 80 default_server',
 line   => 'rewrite ^/redirect_me http://www.staggeringbeauty.com/;',
 }
