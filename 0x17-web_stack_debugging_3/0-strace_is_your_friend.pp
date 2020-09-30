f2760445b3e6dec8b9cd3145262a4a15bbc82390
Cholado <sntgmtnz@gmail.com>
2020-09-30 17:48:05 -0500

    the p stand for pizza

---
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -0,0 +1,5 @@
+# code error 500 in Apache, puppet manifest fix typo php has trailing extra p
+exec { 'fix-wordpress':
+  command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
+  path    => '/usr/local/bin/:/bin/'
+}
